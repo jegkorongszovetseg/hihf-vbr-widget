28776f8ab21faedbc9808f9d53e69eec0b7cb1f9
Akos Stegner <a.stegner@mito.hu>
2020-11-12 20:36:32 +0100

    Develop Error Notice icon

--- src/components/ErrorNotice.vue
+++ src/components/ErrorNotice.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="error-notice">
+    <IconWarning class="icon" width="20" height="20"></IconWarning>
     <span>{{ error }}</span>
   </div>
 </template>
 
 <script>
+import IconWarning from './icons/IconWarning';
+
 export default {
   name: 'ErrorNotice',
+
+  components: {
+    IconWarning
+  },
+
   props: {
     error: {
       type: String,

a5cd702bff35d6b83bdc0b76825c266d6604b820
Akos Stegner <a.stegner@mito.hu>
2021-08-24 21:49:21 +0200

    Prod contition for plugins

--- vue.config.js
+++ vue.config.js
@@ -2,39 +2,43 @@ const WebpackAutoInject = require('webpack-auto-inject-version');
 const CompressionPlugin = require('compression-webpack-plugin');
 const BrotliPlugin = require('brotli-webpack-plugin');
 
+const prod = process.env.NODE_ENV === 'production';
+
 module.exports = {
   productionSourceMap: false,
   configureWebpack: {
-    plugins: [
-      new WebpackAutoInject({
-        PACKAGE_JSON_PATH: './package.json',
-        SHORT: 'MJSZ-VBR-WIDGET',
-        components: {
-          InjectAsComment: true
-        },
-        componentsOptions: {
-          InjectAsComment: {
-            tag: 'Build version: {version} - {date}',
-            dateFormat: 'yyyy-mm-dd HH:mm:ss',
-            multiLineCommentType: false
+    ...(prod && {
+      plugins: [
+        new WebpackAutoInject({
+          PACKAGE_JSON_PATH: './package.json',
+          SHORT: 'MJSZ-VBR-WIDGET',
+          components: {
+            InjectAsComment: true
+          },
+          componentsOptions: {
+            InjectAsComment: {
+              tag: 'Build version: {version} - {date}',
+              dateFormat: 'yyyy-mm-dd HH:mm:ss',
+              multiLineCommentType: false
+            }
           }
-        }
-      }),
+        }),
 
-      new CompressionPlugin({
-        filename: '[path][base].gz',
-        algorithm: 'gzip',
-        test: /\.js$|\.css$|\.html$/,
-        threshold: 10240,
-        minRatio: 0.7
-      }),
+        new CompressionPlugin({
+          filename: '[path][base].gz',
+          algorithm: 'gzip',
+          test: /\.js$|\.css$|\.html$/,
+          threshold: 10240,
+          minRatio: 0.7
+        }),
 
-      new BrotliPlugin({
-        asset: '[path].br[query]',
-        test: /\.js$|\.css$|\.html$/,
-        threshold: 10240,
-        minRatio: 0.7
-      })
-    ]
+        new BrotliPlugin({
+          asset: '[path].br[query]',
+          test: /\.js$|\.css$|\.html$/,
+          threshold: 10240,
+          minRatio: 0.7
+        })
+      ]
+    })
   }
 };
